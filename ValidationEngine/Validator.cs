0e73af2b29837cec64381ec9320a9913e141b0dc
ali-Alrubaye <ali_nasrat@hotmail.com>
2017-05-23 22:46:03 +0200

    Exercises 1 provat med olika input  & Exception allt OK.

---
+++ ValidationEngine/Validator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ValidationEngine
+{
+   public class Validator
+   {
+        //public string Mail = @"^([0-9a-zA-Z])*@([0-9a-zA-Z]).([0-9a-zA-Z])";
+        public string Mail = @"^([0-9a-zA-Z]([-\.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$";
+
+        public bool ValidateEmailAddress(string valid)
+       {
+           if (string.IsNullOrEmpty(valid))
+                throw new InvalidContentsException("You must specify an email address");
+           var valide = Regex.IsMatch(valid, Mail);
+           return valide;
+       }
+
+       
+    }
+}
---
+++ ValidationEngineTests/ValidationTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ValidationEngine;
+
+namespace ValidationEngineTests
+{
+    [TestFixture]
+    public class ValidationTests
+    {
+        [Test]
+        public void TrueForValidAddress()
+        {
+            var sut = new Validator();
+            
+            Assert.IsTrue(sut.ValidateEmailAddress("mike@edument.se"));
+            Assert.IsFalse(sut.ValidateEmailAddress("name@test"));
+            Assert.IsFalse(sut.ValidateEmailAddress("test.test@com"));
+            Assert.IsTrue(sut.ValidateEmailAddress("Name2015@test.com"));
+            Assert.IsTrue(sut.ValidateEmailAddress("name@test2015.com"));
+        }
+
+        //[Test]
+        //public void IfMailHasADotBeforeReturnFalse()
+        //{
+        //    var sut = new Validator();
+        //    var r = sut.ValidateEmailAddress("test.test@com");
+        //    Assert.IsFalse(r);
+        //}
+        [Test]
+        public void ThrowCustomException()
+        {
+            var sut = new Validator();
+            //sut.ValidateEmailAddress("");
+            Assert.Throws<InvalidContentsException>(() => { sut.ValidateEmailAddress(""); });
+            //Assert.Fail("Expected exception not thrown");
+        }
+    }
+}
